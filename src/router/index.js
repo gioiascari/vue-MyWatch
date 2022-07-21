2a8c6401daa4f49c90021c8c58650e438dd3109f
gioiascari <gioia.ascari96@gmail.com>
2022-07-21 14:21:59 +0200

    add router

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+//Import
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+
+Vue.use(VueRouter);
+//Routes's name
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    props: { name: "face" },
+  },
+  {
+    path: "/:name",
+    name: "Step",
+    component: Home,
+    props: true,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
