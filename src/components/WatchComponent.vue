b6ae25835899c72614da5d781ad1bdcdb22d74de
gioiascari <gioia.ascari96@gmail.com>
2022-07-21 17:17:16 +0200

    add watch component

---
+++ src/components/WatchComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg :style="cssVars"></svg>
+</template>
+
+<script>
+export default {
+  name: "WatchComponent",
+  computed: {
+    //Funzione che restituisce valori all'interno di variabili css
+    cssVars() {
+      return {
+        "--bg-strap": this.$store.state.colors.strap.selected.color,
+        "--bg-loop": this.$store.state.colors.loop.selected.color,
+        "--bg-face": this.$store.state.colors.face.selected.color,
+        "--bg-hands": this.$store.state.colors.hands.selected.color,
+      };
+    },
+  },
+};
+</script>
+
+<style></style>
