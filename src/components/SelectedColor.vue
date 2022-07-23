462817070d381da7592df41ab163fadfa059fa21
gioiascari <gioia.ascari96@gmail.com>
2022-07-23 17:15:53 +0200

    add function get colors

--- src/components/SelectedColor.vue
+++ src/components/SelectedColor.vue
@@ -1,10 +1,30 @@
 <template>
-  <div></div>
+  <ul id="selectedColor">
+    <li></li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "SelectedColor",
+  computed: {
+    steps: function () {
+      return this.$store.state.stepsActivated;
+    },
+  },
+
+  getColors: function () {
+    let colors = [];
+    for (let i = 0; i < this.steps.length; i++) {
+      //Ad ogni step che trova va a pushare il nome ed il colore cliccato
+      colors.push({
+        name: this.steps[i],
+        color: this.$store.state.colors[this.steps[i]].selected.color,
+        colorName: this.$store.state.colors[this.steps[i]].selected.name,
+      });
+    }
+    return colors;
+  },
 };
 </script>
 
